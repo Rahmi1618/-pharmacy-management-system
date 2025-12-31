4f0278e4eccee4e287d7601b47076d1a526a30b3
Mindelah <jmindelah@gmail.com>
2025-12-31 23:45:24 +0300

    her it is mindilah's projust

---
+++ Package1/Inventory.java
@@ -0,0 +1,42 @@
+package Package1;
+
+
+/**
+ * @author mindelah
+ * @version 1.0
+ * @created 25-Dec-2025 10:43:59 PM
+ */
+public class Inventory {
+
+	private medicine List medicine;
+	private int total Item;
+
+	public Inventory(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public medicine addMedicine(){
+		return null;
+	}
+
+	public string findMedicineById(){
+		return "";
+	}
+
+	public getLowStockMedicine(){
+
+	}
+
+	public string removeMedicine(){
+		return "";
+	}
+
+	public string updatestock(){
+		return "";
+	}
+
+}
---
+++ Package1/salesAssistant.java
@@ -0,0 +1,31 @@
+package Package1;
+
+
+/**
+ * @author mindelah
+ * @version 1.0
+ * @created 25-Dec-2025 10:44:00 PM
+ */
+public class salesAssistant {
+
+	public salesAssistant(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void generateReceipt(){
+
+	}
+
+	public void handleCustomerQuery(){
+
+	}
+
+	public processSales(){
+
+	}
+
+}
